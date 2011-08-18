0d594020f0715ef7cbcdb15b82688eed48c9302b
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2011-08-18 15:20:17 +0200

    DELETE & UPDATE queries ignored nested calls, this has now been fixed.

--- classes/query.php
+++ classes/query.php
@@ -520,9 +520,12 @@ class Query {
 					continue;
 				}
 
-				if ( ! empty($conditional) and strpos($conditional[0], $this->alias.'.') === 0)
+				if (empty($conditional) or strpos($conditional[0], $this->alias.'.') === 0)
 				{
-					$type != 'select' and $conditional[0] = substr($conditional[0], strlen($this->alias.'.'));
+					if ( ! empty($conditional) and $type != 'select')
+					{
+						$conditional[0] = substr($conditional[0], strlen($this->alias.'.'));
+					}
 					call_user_func_array(array($query, $method), $conditional);
 					unset($where[$key]);
 				}
