b9cb9a9857e3a6ca3b203492ae8048b1a5e325de
Phil Lavin <phil@propcom.co.uk>
2011-10-12 16:55:29 +0200

    Releated #101 - Include fieldset in \Orm\ValidationFailed exception

--- classes/observer/validation.php
+++ classes/observer/validation.php
@@ -13,7 +13,21 @@
 namespace Orm;
 
 // Exception to throw when validation failed
-class ValidationFailed extends \FuelException {}
+class ValidationFailed extends \FuelException {
+	protected $fieldset;
+	
+	public function __construct($message = null, $code = 0, \Exception $previous = null, \Fieldset $fieldset = null)
+	{
+		parent::__construct($message, $code, $previous);
+		
+		$this->fieldset = $fieldset;
+	}
+
+	public function get_fieldset()
+	{
+		return $this->fieldset;
+	}
+}
 
 class Observer_Validation extends Observer
 {
@@ -101,7 +115,8 @@ class Observer_Validation extends Observer
 	 */
 	public function validate(Model $obj)
 	{
-		$val = static::set_fields($obj)->validation();
+		$fieldset = static::set_fields($obj);
+		$val = $fieldset->validation();
 
 		// only allow partial validation on updates, specify the fields for updates to allow null
 		$allow_partial = $obj->is_new() ? false : array();
@@ -118,7 +133,7 @@ class Observer_Validation extends Observer
 
 		if ( ! empty($input) and $val->run($input, $allow_partial, array($obj)) === false)
 		{
-			throw new ValidationFailed($val->show_errors());
+			throw new ValidationFailed($val->show_errors(), 0, null, $fieldset);
 		}
 		else
 		{
