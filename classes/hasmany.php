8efd675f25364d4053c386f410364ff2bb5e8f4e
Chad Hutchins <hutchins.chad@gmail.com>
2012-05-14 12:14:54 -0500

    Added code to respect relationship condition definitions when related records are being retrieved on has-many relationship.

--- classes/hasmany.php
+++ classes/hasmany.php
@@ -48,6 +48,13 @@ class HasMany extends Relation
 			$query->where(current($this->key_to), $from->{$key});
 			next($this->key_to);
 		}
+		
+		foreach (\Arr::get($this->conditions, 'where', array()) as $key => $condition)
+		{
+			!is_array($condition) and $condition = array($key, '=', $condition);
+			$query->where($condition);
+		}
+		
 		return $query->get();
 	}
 
