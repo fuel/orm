fdd38b4e11a2880b8384029e2ec48679fe8054e9
Adam Kusmierz <kusmierz@gmail.com>
2012-06-11 19:25:39 +0200

    #169

--- classes/belongsto.php
+++ classes/belongsto.php
@@ -54,6 +54,14 @@ class BelongsTo extends Relation
 			$query->where(current($this->key_to), $from->{$key});
 			next($this->key_to);
 		}
+
+		$query->order_by(\Arr::get($this->conditions, 'order_by') ?: \Arr::get($this->conditions, 'order_by', array()));
+		foreach (\Arr::get($this->conditions, 'where', array()) as $key => $condition)
+		{
+			! is_array($condition) and $condition = array($key, '=', $condition);
+			$query->where($condition);
+		}
+		
 		return $query->get_one();
 	}
 
