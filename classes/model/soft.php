4bdbf696f3379b2808545f04ab068e42ab3ab640
Steve West <steve.west@veinteractive.com>
2012-11-05 11:05:32 +0000

    Made it possible to filter out soft deleted entries.

--- classes/model/soft.php
+++ classes/model/soft.php
@@ -12,7 +12,34 @@ namespace Orm;
 class Model_Soft extends Model
 {
 	
+	/**
+	 * Default column name that contains the deleted timestamp
+	 * @var string
+	 */
+	private static $_default_field_name = 'delete_at';
+	/**
+	 * Default value for if a mysql timestamp should be used.
+	 * @var boolean
+	 */
+	private static $_default_mysql_timestamp = true;
+	
+	/**
+	 * Contains cached soft delete properties.
+	 * @var array
+	 */
 	protected static $_soft_delete_cached = array();
+	
+	/**
+	 * Allows results to be filtered to hide soft deleted entries.
+	 */
+	protected static $_conditions = array(
+		'where' => array(),
+	);
+	
+	public static function _init()
+	{
+		
+	}
 	
 	/**
 	 * Gets the soft delete properties.
@@ -69,14 +96,27 @@ class Model_Soft extends Model
 	/**
 	 * Updates the defined deleted_field with a current timestamp rather than
 	 * deleting.
+	 * 
+	 * @return this
 	 */
 	public function delete($cascade = null, $use_transaction = false)
 	{
-		$deletedColumn = static::soft_delete_property('deleted_field', 'deleted_at');
-		$mysql_timestamp = static::soft_delete_property('mysql_timestamp', true);
+		$deletedColumn = static::soft_delete_property('deleted_field', self::$_default_field_name);
+		$mysql_timestamp = static::soft_delete_property('mysql_timestamp', self::$_default_mysql_timestamp);
 		
 		$this->{$deletedColumn} = $mysql_timestamp ? \Date::forge()->format('mysql') : \Date::forge()->get_timestamp();
 		
 		$this->save();
+		
+		return $this;
+	}
+	
+	public static function find($id = null, array $options = array())
+	{
+		//Make sure we are filtering out soft deleted items
+		$deletedColumn = static::soft_delete_property('deleted_field', self::$_default_field_name);
+		static::$_conditions['where'][] = array($deletedColumn, null);
+		
+		return parent::find($id, $options);
 	}
 }
