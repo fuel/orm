3a25ba31e851d08fb1df94e93e20f472a58d3e33
Steve West <steve.west@veinteractive.com>
2012-11-06 16:43:52 +0000

    Comments and code cleanup

--- classes/model/temporal.php
+++ classes/model/temporal.php
@@ -9,13 +9,12 @@ namespace Orm;
  */
 class Model_Temporal extends Model
 {
-	
+
 	protected static $_temporal_cached = array();
-	
 	private static $_default_mysql_timestamp = true;
 	private static $_default_timestamp_field = 'temporal';
 	private static $_pk_check_disabled = array();
-	
+
 	/**
 	 * Gets the temporal properties.
 	 * Mostly stolen from the parent class properties() function
@@ -46,7 +45,7 @@ class Model_Temporal extends Model
 
 		return static::$_temporal_cached[$class];
 	}
-	
+
 	/**
 	 * Fetches a soft delete property description array, or specific data from it.
 	 * Stolen from parent class.
@@ -67,25 +66,37 @@ class Model_Temporal extends Model
 
 		return \Arr::get(static::$_temporal_cached[$class], $key, $default);
 	}
-	
-	//find revision
-	public static function find_revision($id, $timestamp=null)
+
+	/**
+	 * Finds a specific revision for the given ID. If a timestamp is specified 
+	 * the revision returned will reflect the entity's state at that given time.
+	 * 
+	 * @param type $id
+	 * @param int $timestamp
+	 * @return type
+	 */
+	public static function find_revision($id, $timestamp = null)
 	{
-		if($timestamp == null)
+		if ($timestamp == null)
 			$timestamp = 0;
-		
+
 		return static::find(array($id, $timestamp));
 	}
-	
-	//save
+
+	/**
+	 * Overrides the save method to allow temporal models to be 
+	 * @param type $cascade
+	 * @param type $use_transaction
+	 * @return type
+	 */
 	public function save($cascade = null, $use_transaction = false)
 	{
 		//Load temporal properties.
 		$timestamp_field = static::temporal_property('timestamp_name', self::$_default_timestamp_field);
 		$mysql_timestamp = static::temporal_property('mysql_timestamp', self::$_default_mysql_timestamp);
-		
+
 		//If this is new then just call the parent and let everything happen as normal
-		if($this->is_new())
+		if ($this->is_new())
 		{
 			return parent::save($cascade, $use_transaction);
 		}
@@ -93,57 +104,71 @@ class Model_Temporal extends Model
 		else
 		{
 			$diff = $this->get_diff();
-			
-			if(count($diff[0]) > 0)
+
+			if (count($diff[0]) > 0)
 			{
 				//Take a copy before resetting
 				$newModel = clone $this;
-				
+
 				//Reset the current model and update the timestamp
 				$this->reset();
-				
+
 				self::disable_primary_key_check();
 				$this->{$timestamp_field} = $mysql_timestamp ? \Date::forge()->format('mysql') : \Date::forge()->get_timestamp();
 				self::enable_primary_key_check();
-				
+
 				parent::save();
-				
+
 				//Construct a copy of this model and save that with a 0 timestamp
 				$newModel->id = $this->id; //TODO: unhardcode this
 				$newModel->{$timestamp_field} = 0;
-				
+
 				return $newModel->save();
 			}
 		}
 	}
-	
+
+	/**
+	 * Overrides the parent primary_key method to allow primaray key enforcement
+	 * to be turned off when updating a temporal model.
+	 */
 	public static function primary_key()
 	{
 		$class = get_called_class();
-		
-		if(static::get_primary_key_status())
+
+		if (static::get_primary_key_status())
 		{
 			return static::$_primary_key;
 		}
-		
+
 		return array();
 	}
-	
+
+	/**
+	 * Disables PK checking
+	 */
 	private static function disable_primary_key_check()
 	{
 		$class = get_called_class();
 		self::$_pk_check_disabled[$class] = false;
 	}
-	
+
+	/**
+	 * Enables PK checking
+	 */
 	private static function enable_primary_key_check()
 	{
 		$class = get_called_class();
 		self::$_pk_check_disabled[$class] = true;
 	}
-	
+
+	/**
+	 * Returns true if the PK checking should be performed. Defaults to true
+	 */
 	private static function get_primary_key_status()
 	{
 		$class = get_called_class();
 		return \Arr::get(self::$_pk_check_disabled, $class, true);
 	}
+
 }
