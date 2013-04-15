fuel/orm

commit 8e783468d6ddfe7a0b398872c17f14b7385c9223

fixed notice error in get_diff(). Closes #233

classes/model.php
@@ -1527,9 +1527,10 @@ class Model implements \ArrayAccess, \Iterator
 			$rel = static::relations($key);
 			if ($rel->singular)
 			{
+				$new_pk = $val->implode_pk($val);
 				if (empty($this->_original_relations[$key]) !== empty($val)
 					or ( ! empty($this->_original_relations[$key])
-					and $new_pk = $val->implode_pk($val)
+					and $new_pk
 					and $this->_original_relations[$key] !== $new_pk))
 				{
 					$diff[0][$key] = isset($this->_original_relations[$key]) ? $this->_original_relations[$key] : null;

commit 03f3e9f01019b71e117feeeb1b38b941dd4c5526

fixed get_diff(), broken in 8e783468d6ddfe7a0b398872c17f14b7385c9223

classes/model.php
@@ -1527,11 +1527,10 @@ class Model implements \ArrayAccess, \Iterator
 			$rel = static::relations($key);
 			if ($rel->singular)
 			{
-				$new_pk = $val->implode_pk($val);
 				if (empty($this->_original_relations[$key]) !== empty($val)
-					or ( ! empty($this->_original_relations[$key])
-					and $new_pk
-					and $this->_original_relations[$key] !== $new_pk))
+					or ( ! empty($this->_original_relations[$key]) and ! empty($val)
+						and $this->_original_relations[$key] !== $new_pk = $val->implode_pk($val)
+					))
 				{
 					$diff[0][$key] = isset($this->_original_relations[$key]) ? $this->_original_relations[$key] : null;
 					$diff[1][$key] = isset($val) ? $new_pk : null;