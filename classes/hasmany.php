6940f7f38c056a63bd3da3180871129c1031b16c
WanWizard <wanwizard@wanwizard.eu>
2013-04-21 14:45:04 +0200

    bumped the docblock version to 1.6

--- classes/hasmany.php
+++ classes/hasmany.php
@@ -1,9 +1,11 @@
 <?php
 /**
+ * Fuel
+ *
  * Fuel is a fast, lightweight, community driven PHP5 framework.
  *
  * @package    Fuel
- * @version    1.5
+ * @version    1.6
  * @author     Fuel Development Team
  * @license    MIT License
  * @copyright  2010 - 2013 Fuel Development Team
