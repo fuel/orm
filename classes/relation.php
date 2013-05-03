872ccc7b2dd39e040a8b5189833784306cf33323
WanWizard <wanwizard@wanwizard.eu>
2013-05-03 13:18:20 +0200

    removed irrelevant docblock comment

--- classes/relation.php
+++ classes/relation.php
@@ -115,7 +115,6 @@ abstract class Relation
 	 * @param  array|Model  single or multiple model instances to save
 	 * @param  bool         whether the model_from has been saved already
 	 * @param  null|bool    either uses default setting (null) or forces when true or prevents when false
-	 * @todo   make abstract
 	 */
 	abstract public function save($model_from, $model_to, $original_model_id, $parent_saved, $cascade);
 
@@ -126,7 +125,6 @@ abstract class Relation
 	 * @param  array|Model  single or multiple model instances to delete
 	 * @param  bool         whether the model_from has been saved already
 	 * @param  null|bool    either uses default setting (null) or forces when true or prevents when false
-	 * @todo   make abstract
 	 */
 	abstract public function delete($model_from, $model_to, $parent_deleted, $cascade);
 
