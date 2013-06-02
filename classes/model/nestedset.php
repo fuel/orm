f29e90bda5ff1900ea47660ce6e89e79fd14f1c9
WanWizard <wanwizard@wanwizard.eu>
2013-06-02 17:25:59 +0200

    if using objects, simply return $this, children are added to existing objects

--- classes/model/nestedset.php
+++ classes/model/nestedset.php
@@ -804,7 +804,7 @@ class Model_Nestedset extends Model
 			}
 		}
 
-		return $tree;
+		return $as_object ? $this : $tree;
 	}
 
 	// -------------------------------------------------------------------------
