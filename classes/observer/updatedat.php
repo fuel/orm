8dc62545f3b3d7b2653b072e6c7890c8ead60f7c
stevewest <uruwolf@gmail.com>
2013-10-21 10:40:09 +0100

    Adds the ability to include relations when using the UpdatedAt observer

--- classes/observer/updatedat.php
+++ classes/observer/updatedat.php
@@ -40,6 +40,8 @@ class Observer_UpdatedAt extends Observer
 	 */
 	protected $_property;
 
+	protected $_relations;
+
 	/**
 	 * Set the properties for this observer instance, based on the parent model's
 	 * configuration or the defined defaults.
@@ -51,6 +53,7 @@ class Observer_UpdatedAt extends Observer
 		$props = $class::observers(get_class($this));
 		$this->_mysql_timestamp  = isset($props['mysql_timestamp']) ? $props['mysql_timestamp'] : static::$mysql_timestamp;
 		$this->_property         = isset($props['property']) ? $props['property'] : static::$property;
+		$this->_relations        = isset($props['relations']) ? $props['relations'] : array();
 	}
 
 	/**
@@ -70,9 +73,49 @@ class Observer_UpdatedAt extends Observer
 	 */
 	public function before_update(Model $obj)
 	{
-		if ($obj->is_changed())
+		// If there are any relations loop through and check if any of them have been changed
+		$relation_changed = false;
+		foreach ( $this->_relations as $relation)
+		{
+			if ($this->relation_changed($obj, $relation))
+			{
+				$relation_changed = true;
+				continue;
+			}
+		}
+
+		if ($obj->is_changed() || $relation_changed)
 		{
 			$obj->{$this->_property} = $this->_mysql_timestamp ? \Date::time()->format('mysql') : \Date::time()->get_timestamp();
 		}
+	}
+
+	/**
+	 * Checks to see if any models in the given relation are changed. This function is lazy so will return true as soon
+	 * as it finds something that has changed.
+	 *
+	 * @param Model  $obj
+	 * @param string $relation
+	 *
+	 * @return bool
+	 */
+	protected function relation_changed(Model $obj, $relation)
+	{
+		// Check that the relation exists
+		if ($obj->relations($relation) === false)
+		{
+			throw new \InvalidArgumentException('Unknown relation '.$relation);
+		}
+
+		// Now we know the relation exists loop through all the models and see if anything has changed
+		foreach ($obj->{$relation} as $related_model)
+		{
+			if ($related_model->is_changed())
+			{
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
