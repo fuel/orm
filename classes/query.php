71a5d3f5308f90970d49cd6eec4b080105417be8
AdamSGit <adamelio@protonmail.com>
2020-09-09 15:14:37 +0200

    Fix issue with nested relationship

--- classes/query.php
+++ classes/query.php
@@ -1621,7 +1621,7 @@ class Query
 					{
 						$target =& $target[$parent];
 					}
-					else
+					else if ($target)
 					{
 						$fullrel .= '.'.$parent;
 						$target =& $target[$parent][$rowpks[$fullrel]];
